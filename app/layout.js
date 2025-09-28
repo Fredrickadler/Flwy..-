dc157e64b52bb8d488d6c2d17180f165b26e913d
Fredrickadler <13fredrick2001@gmail.com>
2025-09-28 22:13:20 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,37 @@
 import "./globals.css";
-import dynamic from "next/dynamic";
 
-const WagmiProvider = dynamic(() => import("~/components/providers/WagmiProvider"), { ssr: false });
+export const metadata = {
+  title: "Flwy Frame",
+  description: "A Farcaster Frame App",
+  icons: {
+    icon: "/icon.png", // باید فایل icon.png رو توی public بذاری
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <WagmiProvider>{children}</WagmiProvider>
-      </body>
+      <head>
+        {/* متای لازم برای Farcaster Frame */}
+        <meta
+          name="fc:frame"
+          content='{
+            "version":"next",
+            "imageUrl":"https://flwy.vercel.app/preview.png",
+            "button":{
+              "title":"Open Flwy",
+              "action":{
+                "type":"launch_frame",
+                "name":"Flwy",
+                "url":"https://flwy.vercel.app",
+                "splashImageUrl":"https://flwy.vercel.app/splash.png",
+                "splashBackgroundColor":"#000000"
+              }
+            }
+          }'
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
