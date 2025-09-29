b4098c01c91d8f9ea4ab2e412b8b82a804a09eb6
Fredrickadler <13fredrick2001@gmail.com>
2025-09-29 14:49:46 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,9 @@ export default function RootLayout({ children }) {
           }'
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
