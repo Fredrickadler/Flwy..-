6d423f796a0da23ca2f4481dbe480c28687733d7
Fredrickadler <13fredrick2001@gmail.com>
2025-09-29 15:57:46 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 
 export const metadata = {
   title: "Flwy Frame",
-  description: "A Farcaster Frame App",
+  description: "A Farcaster Mini App with custom graphics",
   icons: {
     icon: "/icon.png",
   },
@@ -11,25 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="fc:frame"
-          content='{
-            "version":"next",
-            "imageUrl":"https://flwy-one.vercel.app/preview.png",
-            "button":{
-              "title":"Open Flwy",
-              "action":{
-                "type":"launch_frame",
-                "name":"Flwy",
-                "url":"https://flwy-one.vercel.app",
-                "splashImageUrl":"https://flwy-one.vercel.app/splash.png",
-                "splashBackgroundColor":"#000000"
-              }
-            }
-          }'
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
